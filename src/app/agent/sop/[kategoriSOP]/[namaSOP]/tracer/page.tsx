435bc15edba5d091942ff9f8679fd440f034964f
alghifrz <alghifarirasyidzola@gmail.com>
2025-11-11 09:12:23 +0700

    change color bg

--- src/app/agent/sop/[kategoriSOP]/[namaSOP]/tracer/page.tsx
+++ src/app/agent/sop/[kategoriSOP]/[namaSOP]/tracer/page.tsx
@@ -17,7 +17,7 @@ export default async function SOPTracerPage({ params }: SOPTracerPageProps) {
   return (
     <ProtectedRoute>
       <Layout>
-        <div className="min-h-screen bg-gray-50">
+        <div className="min-h-screen">
           <div className="max-w-7xl mx-auto px-6 py-16">
             <SOPTracerContent 
               kategoriSOP={decodedKategoriSOP} 
